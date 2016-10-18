f5dc33c067cd59e69157d7373527d2adde8ca47b
Joshua Haupt <jhaupt@siue.edu>
2016-10-17 23:49:52 -0500

    Expr.g4

--- HW4/ExprJoyRide.java
+++ HW4/ExprJoyRide.java
@@ -7,13 +7,13 @@ public class ExprJoyRide {
 	public static void main(String[] args) throws Exception {
 		String inputFile = null;
 		if ( args.length>0 ) inputFile = args[0];
-	InputStream is = System.in;
-	if ( inputFile!=null ) is = new FileInputStream(inputFile);
-	ANTLRInputStream input = new ANTLRInputStream(is);
-	ExprLexer lexer = new ExprLexer(input);
-	CommonTokenStream tokens = new CommonTokenStream(lexer);
-	ExprParser parser = new ExprParser(tokens);
-	ParseTree tree = parser.prog(); // parse; start at prog
-	System.out.println(tree.toStringTree(parser)); // print tree as text
+		InputStream is = System.in;
+		if ( inputFile!=null ) is = new FileInputStream(inputFile);
+		ANTLRInputStream input = new ANTLRInputStream(is);
+		ExprLexer lexer = new ExprLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		ExprParser parser = new ExprParser(tokens);
+		ParseTree tree = parser.prog(); // parse; start at prog
+		System.out.println(tree.toStringTree(parser)); // print tree as text
 	}
 }
