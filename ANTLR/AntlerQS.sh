332e23f3b87b5145f8c2c0a627ae8089ef238a7c
Joshua Haupt <jhaupt@siue.edu>
2016-10-17 23:08:31 -0500

    START HW4

---
+++ ANTLR/AntlerQS.sh
@@ -0,0 +1,5 @@
+cd /usr/local/lib
+sudo wget http://www.antlr.org/download/antlr-4.5.3-complete.jar
+export CLASSPATH=".:/usr/local/lib/antlr-4.5.3-complete.jar:$CLASSPATH"
+alias antlr4='java -jar /usr/local/lib/antlr-4.5.3-complete.jar'
+alias grun='java org.antlr.v4.gui.TestRig'
